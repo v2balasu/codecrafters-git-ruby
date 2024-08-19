0ba7042dbffe4ae3dcb618c7493b65df5d0d1d77
v2balasu <vithuranb@gmail.com>
2024-08-18 21:28:01 -0400

    uncomment starter

--- app/main.rb
+++ app/main.rb
@@ -2,15 +2,15 @@
 puts "Logs from your program will appear here!"
 
 # Uncomment this block to pass the first stage
-#
-# command = ARGV[0]
-# case command
-# when "init"
-#   Dir.mkdir(".git")
-#   Dir.mkdir(".git/objects")
-#   Dir.mkdir(".git/refs")
-#   File.write(".git/HEAD", "ref: refs/heads/main\n")
-#   puts "Initialized git directory"
-# else
-#   raise RuntimeError.new("Unknown command #{command}")
-# end
+
+command = ARGV[0]
+case command
+when "init"
+  Dir.mkdir(".git")
+  Dir.mkdir(".git/objects")
+  Dir.mkdir(".git/refs")
+  File.write(".git/HEAD", "ref: refs/heads/main\n")
+  puts "Initialized git directory"
+else
+  raise RuntimeError.new("Unknown command #{command}")
+end
